1866956388e524bde5992306d8cb9e8f585e287a
salagata <122649005+salagata@users.noreply.github.com>
2024-07-31 14:31:52 -0500

    Added ALU

---
+++ scripts/alu.js
@@ -0,0 +1,17 @@
+function sum(a,b) {
+    return a + b
+}
+
+function minus(a,b) {
+    return a - b
+}
+
+function multiply(a,b) {
+    return a * b
+}
+
+function divided(a,b) {
+    return a / b
+}
+
+export {sum,minus,multiply,divided}
--- scripts/stack.js
+++ scripts/stack.js
@@ -1,2 +1,1 @@
 const stack = [];
-stack.push(5+4);
