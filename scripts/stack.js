822c09dd7a69f9c81aa9919cfc4bb0a12d9e4433
salagata <122649005+salagata@users.noreply.github.com>
2024-07-31 14:46:21 -0500

    added Stack Operations

--- scripts/stack.js
+++ scripts/stack.js
@@ -1,1 +1,11 @@
-const stack = [];
+const stack = []; // [sum(4,5),minus(sum(4,5),2)]
+
+function pushStack(dat) {
+    stack.push(dat)
+}
+
+function popStack() {
+    return stack.pop()
+}
+
+export {stack,pushStack,popStack}
