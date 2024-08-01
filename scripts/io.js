353370923b7436762bc6bd27e09a8bb88eac2a4b
salagata <122649005+salagata@users.noreply.github.com>
2024-07-31 20:21:47 -0500

    Added Postfixed

--- scripts/io.js
+++ scripts/io.js
@@ -3,5 +3,6 @@ const button = document.getElementById("send");
 const output = document.getElementById("output")
 
 button.onclick = () => {
-    output.innerHTML = eval(input.value)
+    output.innerHTML = infixed2postfixed(input.value);
+    console.log(infixed2postfixed(input.value));
 }
